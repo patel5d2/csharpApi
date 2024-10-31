d8b1de6469f005e97976f2dc0d08c0015f516b83
Dharmin Patel <Dharminp976@gmail.com>
2024-10-31 12:54:14 -0400

    my secand chanfe

--- MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344f6c63617a7969cbcdfa64b3fe44288a577734")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
